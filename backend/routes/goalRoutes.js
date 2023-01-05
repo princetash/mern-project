ce5d3c4116b55a6410dc9d16586efccef3282744
princetash <jamesngangatash@gmail.com>
2023-01-05 14:42:13 +0300

    initial commit

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+app.get('/', (req, res)=>{
+    res.status(200).json({message: 'get goals'})
+})
+
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = process.env.PORT || 500
+const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/api/goals', (req, res)=>{
-    res.status(200).json({message: 'get goals'})
-})
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`server started at port ${port}`))
